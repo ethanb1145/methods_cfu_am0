09584dd5c89ae9471a1ea184ebb319e326a1027c
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-06-23 16:50:40 -0600

    Answer problems

--- final_practice.rb
+++ final_practice.rb
@@ -6,30 +6,61 @@
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
+# What is the return value of your method? The return value is nil. 
+# How many arguments did you pass your method? No arguments.
+
+def greeting
+    puts "Hello, how are you?"
+end
+
+greeting
 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? Return value is "Hello, how are you John?"
+# How many arguments did you pass your method? One.
+# What data type was your argument(s)? A string.
 
+name = "John"
+
+def custom_greeting(name)
+    puts "Hello, how are you #{name}?"
+end
+
+custom_greeting(name)
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? Hello, John Wayne Dwayne
+# How many arguments did you pass your method? Three arguments
+# What data type was your argument(s)? String data types
 
+first = "John"
+middle = "Dwayne"
+last = "Wayne"
+
+def greet_person(first,middle,last)
+    puts "Hello, #{first} #{middle} #{last}"
+end
+
+greet_person(first,middle,last)
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
-# What is the return value of your method?
+# What is the return value of your method? Return value was 5.0
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
+
+number = 25
+
+def square(number)
+    Math.sqrt(number)
+end
+
+square(number)
+
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
